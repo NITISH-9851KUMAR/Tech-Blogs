8432e8d8984b94e39077d2344799b7c9c0d2abad
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-11-18 20:26:58 +0530

    Message Entities

---
+++ src/main/java/entities/Message.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class Message {
+
+    String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
+
+
+
