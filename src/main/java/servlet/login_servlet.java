58f0f83fe422767564639e86196da5608bd6d8fb
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-11-18 15:23:17 +0530

    Login Servlet

--- src/main/java/servlet/login_servlet.java
+++ src/main/java/servlet/login_servlet.java
@@ -1,10 +1,14 @@
 package servlet;
 
+import dao.*;
+import entities.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -17,11 +21,25 @@ public class login_servlet extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out= response.getWriter();
 
-        String uName= request.getParameter("user_name");
-        String password= request.getParameter("password");
+        String uName= request.getParameter("email");
+        String UPassword= request.getParameter("password");
 
+        // call the login Dao
 
+        User user= new User();
 
+        user= new loginDao().getUserByUserNameAndPassword(uName, UPassword);
+
+        if(user==null){ // if login error
+            // means login error, it may userName or password could be wrong
+            System.out.println("Invalid Details");
+        }
+        else{ // if login success
+            // declare a session object
+            HttpSession session= request.getSession();
+            session.setAttribute("CurrentUser", user);
+            response.sendRedirect("profile.jsp"); // send it profile page
+        }
     }
 
 }
