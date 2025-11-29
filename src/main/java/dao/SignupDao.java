e008a41ddc1ad570becce99115b6162b074e4d04
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-11-29 19:49:28 +0530

    update user Details

--- src/main/java/dao/SignupDao.java
+++ src/main/java/dao/SignupDao.java
@@ -17,13 +17,14 @@ public class SignupDao {
         boolean flag = false;
         try {
 
-            String query = "INSERT INTO USER_Details(user_name, email, gender, password, reg_date) VALUES(?, ?, ?, ?, ?)";
+            String query = "INSERT INTO USER_Details(user_name, email, gender, password, reg_date, pic) VALUES(?, ?, ?, ?, ?, ?)";
             PreparedStatement pstm = con.prepareStatement(query);
             pstm.setString(1, user.getUser_name());
             pstm.setString(2, user.getEmail());
             pstm.setString(3, user.getGender());
             pstm.setString(4, user.getPassword());
             pstm.setString(5, user.getBlog_date());
+            pstm.setString(6, user.getImage());
 
             pstm.executeUpdate();
             flag = true;
---
+++ src/main/java/servlet/UpdateUserServlet.java
@@ -0,0 +1,74 @@
+package servlet;
+
+import entities.Message;
+import entities.User;
+import dao.EditUserDetails;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.Part;
+
+@WebServlet("/edit-servlet")
+@MultipartConfig
+public class UpdateUserServlet extends HttpServlet{
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+        PrintWriter out= response.getWriter();
+        response.setContentType("text/html");
+
+        // Fetch User data
+        int userId= Integer.parseInt(request.getParameter("u_id"));
+        String name= request.getParameter("u_name").trim();
+        String email= request.getParameter("u_email").trim();
+        String gender= request.getParameter("u_gender").trim();
+        String password= request.getParameter("u_password").trim();
+
+        // Take the profile pic details
+        Part photoPart= request.getPart("u_pic");
+        String imageName = photoPart.getSubmittedFileName();
+
+        // get the current user details
+        HttpSession session= request.getSession();
+        User user= (User) session.getAttribute("CurrentUser");
+
+        String oldPicName= user.getImage();
+
+        // Set the value
+        user.setId(userId);
+        user.setUser_name(name);
+        user.setEmail(email);
+        user.setGender(gender);
+        user.setPassword(password);
+        user.setImage(imageName);
+
+        boolean flag= new EditUserDetails().updateDetails(user);
+        if(flag){
+            Message msg= new Message("Update Value Successfully");
+
+            // It gives the path of our image
+            String path= request.getServletContext().getRealPath("/img")+File.separator+user.getImage();
+            String odlPicPath = request.getServletContext().getRealPath("/img")+File.separator+oldPicName;
+            UpdatePhoto.deleteFile(odlPicPath); // delete file
+
+            // call the saveFile for update photo
+            if(UpdatePhoto.saveFile(photoPart.getInputStream(), path)){
+                msg= new Message("Update Value Successfully");
+            }
+            session.setAttribute("message", msg);
+            response.sendRedirect("profile.jsp");
+        }else{
+            Message msg= new Message("Something Went Wrong");
+            session.setAttribute("message", msg);
+            response.sendRedirect("profile.jsp");
+        }
+
+
+    }
+}
