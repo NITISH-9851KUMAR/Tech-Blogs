6b9d666ce690bc3cf45e73a722161e20d8c97f4d
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-12-03 11:18:20 +0530

    Post Data Receive Servlet

--- src/main/java/servlet/DoPostServlet.java
+++ src/main/java/servlet/DoPostServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -24,25 +25,24 @@ public class DoPostServlet extends HttpServlet {
         String postTitle= request.getParameter("postTitle").trim();
         String postContent= request.getParameter("postContent").trim();
         String postCode= request.getParameter("postCode").trim();
-        Part par= request.getPart("postPic");
-        String picName= par.getSubmittedFileName().trim();
+        Part photoPart= request.getPart("postPic");
+        String picName= photoPart.getSubmittedFileName().trim();
 
-//        out.println(catId);
-//        out.println(postTitle);
-//        out.println(postContent);
-//        out.println(postCode);
-//        out.println(picName);
 
         // Get current user id
         HttpSession session= request.getSession();
-        User user= (User) session.getAttribute("currentUser");
+        User user= (User) session.getAttribute("CurrentUser");
 
         Post post= new Post(postTitle, postContent, postCode, picName, catId, user.getId());
 
-        PostDao postDao= new PostDao();
-
-        if(postDao.savePost(post)){
-            out.println("Save Post Details to the Database Successfully!");
+        if(PostDao.savePost(post)){
+            // Also we can write this program outside of if block, it is good thing to write inside this block
+            // Because when our db store successfully all data then we store picImage in local folder
+            // save photo to the local folder
+            String uploadPath= getServletContext().getRealPath("/img");
+            // It save to the Local Folder
+            photoPart.write(uploadPath+File.separator+picName);
+            out.println("Done");
         } else{
             out.println("Something Went Wrong");
         }
